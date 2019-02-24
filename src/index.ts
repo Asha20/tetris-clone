a120f9960a352e73cc8dce2fbaa238abe10000a6
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2019-02-24 09:45:09 +0100

    Add gravity

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import Tetris from "./tetris.js";
 import tetrisCanvas from "./tetris-canvas.js";
 
 const game = new Tetris({
-  controls: () => {
-    /* */
+  gravityDelay: 100,
+  controls: ({ left, right }) => {
+    window.addEventListener("keydown", e => {
+      switch (e.key) {
+        case "ArrowLeft":
+          return left();
+        case "ArrowRight":
+          return right();
+      }
+    });
   },
   render: tetrisCanvas(document.body, 40),
 });
--- src/tetris.ts
+++ src/tetris.ts
@@ -9,11 +9,19 @@ export interface FallingTetromino {
   y: number;
 }
 
-type Actions = any;
-
 interface TetrisOptions {
   render: (tetris: Tetris) => void;
-  controls: (actions: Actions) => void;
+  controls: (c: { left: () => void; right: () => void }) => void;
+  gravityDelay: number;
+}
+
+function fallingTetromino(): FallingTetromino {
+  const index = Math.floor(Math.random() * tetrominoes.length);
+  return {
+    tetromino: tetrominoes[index],
+    x: 5,
+    y: 0,
+  };
 }
 
 export default class Tetris {
@@ -28,12 +36,63 @@ export default class Tetris {
     opts.render(this);
 
     setInterval(() => {
-      this.fallingTetromino.y =
-        (this.fallingTetromino.y + 1) % this.grid.height;
-    }, 500);
+      this.applyGravity();
+    }, opts.gravityDelay);
 
-    window.addEventListener("click", () => {
-      this.fallingTetromino = { tetromino: tetrominoes[1], x: 0, y: 0 };
+    opts.controls({
+      left: () => this.moveTetromino(-1, 0),
+      right: () => this.moveTetromino(1, 0),
     });
+
+    // Quick debug
+    window.addEventListener("click", e => {
+      if (!e.shiftKey) {
+        return;
+      }
+
+      // tslint:disable-next-line no-console
+      console.log(this.grid);
+    });
+  }
+
+  /** Attempt to place the falling tetromino into the grid. */
+  tryMove(deltaX: number, deltaY: number, shouldMerge: boolean) {
+    const shape = this.fallingTetromino.tetromino.currentState;
+
+    const pos: M.Vector = {
+      x: this.fallingTetromino.x + deltaX,
+      y: this.fallingTetromino.y + deltaY,
+    };
+
+    const { matrix, merged } = M.merge(
+      this.grid,
+      shape,
+      pos,
+      (x1, x2) => x1 !== 0 && x2 !== 0,
+      (x1, x2) => x2,
+    );
+
+    if (merged && shouldMerge) {
+      this.grid = matrix;
+    }
+
+    return merged;
+  }
+
+  moveTetromino(deltaX: number, deltaY: number) {
+    const canMove = this.tryMove(deltaX, deltaY, false);
+    if (canMove) {
+      this.fallingTetromino.x += deltaX;
+      this.fallingTetromino.y += deltaY;
+    }
+    return canMove;
+  }
+
+  applyGravity() {
+    const moved = this.moveTetromino(0, 1);
+    if (!moved) {
+      this.tryMove(0, 0, true);
+      this.fallingTetromino = fallingTetromino();
+    }
   }
 }
