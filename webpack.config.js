0aa9ffa0ad0d763c39cc9a06b2409ca28ab45dc4
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2019-03-07 17:01:56 +0100

    Fix source map staying inline in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,39 @@
 const webpack = require("webpack");
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-const PROD = process.env.NODE_ENV === "production";
+module.exports = (_, argv) => {
+  const PROD = argv.mode === "production";
 
-module.exports = {
-  mode: PROD ? "production" : "development",
-  devtool: PROD ? "source-map" : "inline-source-map",
-  entry: "./src/index.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/public",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-      },
-    ],
-  },
-  plugins: [new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])],
+  return {
+    mode: argv.mode || "none",
+    devtool: PROD ? "source-map" : "inline-source-map",
+    entry: "./src/index.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/public",
+      sourceMapFilename: "bundle.map.js",
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+        },
+      ],
+    },
+    optimization: {
+      minimize: PROD,
+      minimizer: [
+        new UglifyJsPlugin({
+          sourceMap: true,
+        }),
+      ],
+    },
+    plugins: [new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])],
+  };
 };
